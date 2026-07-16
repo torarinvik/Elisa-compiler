35aa91f47fca33244b050ffb04c2ec7fdbbb1637
torarinvik <torarinvik@gmail.com>
2026-07-16 23:12:21 +0200

    Validate fixed array shapes

--- test/parity/array_bounds_smoke.sh
+++ test/parity/array_bounds_smoke.sh
@@ -21,5 +21,7 @@ check_case() {
 check_case $'const IDX: usize = 4\n\ndef bad() -> u8:\n    buf: u8[4] = zeroed\n    return buf[IDX]\n' "index out of bounds: 'buf' has 4 elements, index is 4"
 check_case $'def bad() -> view[u8]:\n    buf: u8[4] = zeroed\n    return buf[2:5]\n' 'constant slice end 5 out of bounds for array of length 4'
 check_case $'def bad() -> view[u8]:\n    buf: u8[4] = zeroed\n    return buf[3:1]\n' 'constant slice start 3 is after end 1'
+check_case $'def bad(n: usize) -> void:\n    buf: u8[n] = zeroed\n' 'array size must be a compile-time integer'
+check_case $'def bad(buf: u8[4]) -> u8[5]:\n    return buf\n' 'return type expects u8[5], got u8[4]'
 
 echo "array bounds smoke OK"
